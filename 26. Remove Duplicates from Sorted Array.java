690a0f446dba9f3817da78610f6ff04537584c1e
shaik ashwak <ashwakacchu@gmail.com>
2026-07-21 14:22:36 +0530

    Update 26. Remove Duplicates from Sorted Array.java

--- 26. Remove Duplicates from Sorted Array.java
+++ 26. Remove Duplicates from Sorted Array.java
@@ -3,11 +3,8 @@ class Solution {
         if (nums.length == 0) {
             return 0;
         }
-
-        // Pointer for the position of the last unique element found
         int k = 1; 
-
-        for (int i = 1; i < nums.length; i++) {
+     for (int i = 1; i < nums.length; i++) {
             // Whenever we find a new unique element
             if (nums[i] != nums[i - 1]) {
                 nums[k] = nums[i];
